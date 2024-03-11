4d31fdc286aea9af6f89f94cf8d6d338988019dc
MarasKrafts <goldenprenzy@gmail.com>
2024-03-11 17:00:46 +0100

    Add project files.

---
+++ MyMusicPlayer/MusicPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading.Tasks;
+
+namespace MyMusicPlayer
+{
+    public class MusicPlayer
+    {
+        private List<string> playlist = new List<string>();
+
+        public void CreatePlaylist()
+        {
+            Console.Write("Enter song name to add to playlist: ");
+            string songName = Console.ReadLine();
+            playlist.Add(songName);
+            Console.WriteLine($"{songName} added to playlist.");
+        }
+
+        public void PlayMusic()
+        {
+            Console.WriteLine("Playing music...");
+            // Implement music playback logic (e.g., load and play music files)
+        }
+
+        public void ShuffleMusic()
+        {
+            // Implement shuffle music logic (e.g., randomize playlist order)
+            Console.WriteLine("Shuffling music...");
+        }
+
+        public void DeleteMusic()
+        {
+            Console.Write("Enter song name to delete from playlist: ");
+            string songName = Console.ReadLine();
+            if (playlist.Contains(songName))
+            {
+                playlist.Remove(songName);
+                Console.WriteLine($"{songName} removed from playlist.");
+            }
+            else
+            {
+                Console.WriteLine($"{songName} not found in playlist.");
+            }
+        }
+    }
+
+    
+    
+}
---
+++ MyMusicPlayer/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MyMusicPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MusicPlayer musicPlayer = new MusicPlayer();
+
+            while (true)
+            {
+                Console.WriteLine("Music Player App");
+                Console.WriteLine("1. Create Playlist");
+                Console.WriteLine("2. Play Music");
+                Console.WriteLine("3. Shuffle Music");
+                Console.WriteLine("4. Delete Music");
+                Console.WriteLine("5. Exit");
+
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        musicPlayer.CreatePlaylist();
+                        break;
+                    case 2:
+                        musicPlayer.PlayMusic();
+                        break;
+                    case 3:
+                        musicPlayer.ShuffleMusic();
+                        break;
+                    case 4:
+                        musicPlayer.DeleteMusic();
+                        break;
+                    case 5:
+                        Console.WriteLine("Exiting Music Player App. Goodbye!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
